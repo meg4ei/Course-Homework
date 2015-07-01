5daf74fb43d8a69657e31072df89e690158a6038
meg4ei <meg.glupost@gmail.com>
2015-07-01 16:44:04 +0300

    mmmmmmhhhhb

---
+++ Array2/Array1/Array1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 4]
+            {
+            {5,6,7,8},
+            {11,15,17,20},
+            {15,75,10,14},
+            };
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    if (a[i, j] % 2 == 0)
+                    {
+                        Console.WriteLine(a[i, j] + "\t");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Array2/Array1/Array3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a < b)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
