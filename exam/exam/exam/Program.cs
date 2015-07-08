6257ff54781d991ce29da51844d7d6964e952b09
meg4ei <meg.glupost@gmail.com>
2015-07-08 21:44:35 +0300

    mhmmmmh

---
+++ exam/exam/exam/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a number from 2 to 9 :");
+            int n = int.Parse(Console.ReadLine());
+            int[,] array = new int[n, n];
+            int s= n*n;
+            int z=0;
+            int []b= new int [s];
+            int sum=0;
+
+            if (n>=2 && n < 10)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = int.Parse(Console.ReadLine()); 
+                    }
+                } Console.WriteLine();
+                
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                            b[z] = array[j, i];
+                            Console.WriteLine(b[z]);
+                            sum += b[z];
+                            z++;
+                    }
+                   
+                }
+                Console.WriteLine();
+                Console.WriteLine("The sum is: " + sum);
+            }
+        }
+    }
+}
+
+
